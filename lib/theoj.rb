503bb5d2f7997f30c2c07337ec33a364d0b7222e
Juanjo Bazán <jjbazan@gmail.com>
2021-09-17 12:01:46 +0200

    require all

--- lib/theoj.rb
+++ lib/theoj.rb
@@ -1,2 +1,8 @@
+require_relative "theoj/version"
+require_relative "theoj/github"
+require_relative "theoj/review_issue"
+require_relative "theoj/paper"
+require_relative "theoj/author"
+
 module Theoj
 end
