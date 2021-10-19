dd032f1389f51fed266b2760d41ce0a9daa1a4f6
Juanjo Bazán <jjbazan@gmail.com>
2021-10-19 13:38:35 +0200

    add Author

--- lib/theoj/author.rb
+++ lib/theoj/author.rb
@@ -1,16 +1,94 @@
+require "nameable"
+
 module Theoj
   class Author
     attr_accessor :name
     attr_accessor :orcid
     attr_accessor :affiliation
 
-    def initialize(name, orcid, affiliation)
-      @name = name
-      @orcid = orcid
-      @affiliation = affiliation
+    AUTHOR_FOOTNOTE_REGEX = /^[^\^]*/
+
+    # Initialized with authors & affiliations block in the YAML header from an Open Journal paper
+    # e.g. https://joss.readthedocs.io/en/latest/submitting.html#example-paper-and-bibliography
+    def initialize(name, orcid, index, affiliations_hash)
+      parse_name name
+      @orcid = validate_orcid orcid
+      @affiliation = build_affiliation_string(index, affiliations_hash)
     end
 
-    def validate_orcid
+    def given_name
+      @parsed_name.first
+    end
+
+    def middle_name
+      @parsed_name.middle
+    end
+
+    def last_name
+      @parsed_name.last
+    end
+
+    def initials
+      [@parsed_name.first, @parsed_name.middle].compact.map {|v| v[0] + "."} * ' '
+    end
+
+    def to_h
+      {
+        given_name: given_name,
+        middle_name: middle_name,
+        last_name: last_name,
+        orcid: orcid,
+        affiliation: affiliation
+      }
+    end
+
+    private
+
+    def parse_name(author_name)
+      @parsed_name = Nameable::Latin.new.parse(strip_footnotes(author_name))
+      @name = @parsed_name.to_nameable
+    end
+
+    # Input: Arfon Smith^[Corresponding author: arfon@example.com]
+    # Output: Arfon Smith
+    def strip_footnotes(author_name)
+      author_name.to_s[AUTHOR_FOOTNOTE_REGEX]
+    end
+
+    def validate_orcid(author_orcid)
+      return nil if author_orcid.to_s.strip.empty?
+
+      validator = Theoj::Orcid.new(author_orcid)
+      if validator.valid?
+        return author_orcid.strip
+      else
+        raise "Problem with ORCID (#{author_orcid}) for #{self.name}. #{validator.error}"
+      end
+    end
+
+    # Takes the author affiliation index and a hash of all affiliations and
+    # associates them. Then builds the author affiliation string
+    def build_affiliation_string(index, affiliations_hash)
+      return nil if index.nil? # Some authors don't have an affiliation
+
+      # If multiple affiliations, parse each one and build the affiliation string
+      author_affiliations = []
+
+      # Turn YAML keys into strings so that mixed integer and string affiliations work
+      affiliations_hash.transform_keys!(&:to_s)
+
+      affiliations = index.to_s.split(',').map(&:strip)
+
+      # Raise if we can't parse the string, might be because of this bug :-(
+      # https://bugs.ruby-lang.org/issues/12451
+      affiliations.each do |a|
+        raise "Problem with affiliations for #{self.name}, perhaps the " +
+              "affiliations index need quoting?" unless affiliations_hash.has_key?(a)
+
+        author_affiliations << affiliations_hash[a].strip
+      end
+
+      author_affiliations.join(', ')
     end
 
   end
---
+++ spec/lib/author_spec.rb
@@ -0,0 +1,95 @@
+describe Theoj::Author do
+
+  describe "initialization" do
+    it "should parse name" do
+      name = "Ellen Louise Ripley"
+      author = Theoj::Author.new(name, "0000-0001-2345-6789", nil, {})
+
+      expect(author.name).to eq("Ellen Ripley")
+    end
+
+    it "should remove footnotes from the name string" do
+      name = "Arfon Smith^[Corresponding author: arfon@arfon.arf]"
+      author = Theoj::Author.new(name, "0000-0001-2345-6789", nil, {})
+
+      expect(author.name).to eq("Arfon Smith")
+    end
+
+    it "should build the affiliation string" do
+      affiliations = {1 => "The Open Journals", 2 => "Nostromo", 3 => "Other"}
+      author = Theoj::Author.new("E. Ripley", "0000-0001-2345-6789", 2, affiliations)
+
+      expect(author.affiliation).to eq("Nostromo")
+    end
+
+    it "should accept multiple affiliations" do
+      affiliations = {1 => "The Open Journals", 2 => "Nostromo", 3 => "Other"}
+      author = Theoj::Author.new("E. Ripley", "0000-0001-2345-6789", "1,2", affiliations)
+
+      expect(author.affiliation).to eq("The Open Journals, Nostromo")
+    end
+
+    it "should error if invalid affiliation" do
+      expect {
+        Theoj::Author.new("E. Ripley", "0000-0001-2345-6789", 1, {})
+      }.to raise_error "Problem with affiliations for E. Ripley, perhaps the affiliations index need quoting?"
+    end
+
+    it "should validate the ORCID" do
+      affiliations = {1 => "The Open Journals", 2 => "Nostromo", 3 => "Other"}
+
+      author = Theoj::Author.new("E. Ripley", " 0000-0001-2345-6789  ", "1,2", affiliations)
+      expect(author.orcid).to eq("0000-0001-2345-6789")
+
+      author = Theoj::Author.new("E. Ripley", "  ", "1,2", affiliations)
+      expect(author.orcid).to be_nil
+    end
+
+    it "should error if ORCID is invalid" do
+      expect {
+        Theoj::Author.new("E. Ripley", "0000-0000-0000-0008", nil, {})
+      }.to raise_error "Problem with ORCID (0000-0000-0000-0008) for E. Ripley. Invalid ORCID"
+    end
+  end
+
+  describe "parsed name" do
+    before do
+      name = "Ellen Louise Ripley"
+      @author = Theoj::Author.new(name, "0000-0001-2345-6789", nil, {})
+    end
+
+    it "should assign give_name" do
+      expect(@author.given_name).to eq("Ellen")
+    end
+
+    it "should assign middle_name" do
+      expect(@author.middle_name).to eq("Louise")
+    end
+
+    it "should assign last_name" do
+      expect(@author.last_name).to eq("Ripley")
+    end
+
+    it "should be used to create author's initials" do
+      expect(@author.initials).to eq("E. L.")
+    end
+  end
+
+  describe "#to_h" do
+    it "should return a hash with all the author's info" do
+      name = "Ellen Louise Ripley"
+      affiliations = {1 => "The Open Journals", 2 => "Nostromo", 3 => "Other"}
+      author = Theoj::Author.new(name, "0000-0001-2345-6789", "1,2", affiliations)
+
+      expected_author_info = {
+        given_name: "Ellen",
+        middle_name: "Louise",
+        last_name: "Ripley",
+        orcid: "0000-0001-2345-6789",
+        affiliation: "The Open Journals, Nostromo"
+      }
+
+      expect(author.to_h).to eq(expected_author_info)
+    end
+  end
+end
